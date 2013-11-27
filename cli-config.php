67e7f86fad0663f821d17588a24562dbf958b1c9
alinard <alinard@a53f6af1-6dec-4d6f-9c0f-5ea686734470>
2013-11-27 14:11:55 +0000

    Remove PHAR building.

--- cli-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-
-require_once 'Bootstrap.php';
-
-$helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
-    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($entityManager)
-));
