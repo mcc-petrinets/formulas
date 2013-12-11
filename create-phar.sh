a7f40046b24b4a7846341bb6cc13268916611f8f
alinard <alinard@a53f6af1-6dec-4d6f-9c0f-5ea686734470>
2013-12-11 20:07:14 +0000

    Strengthen create-phar.sh.

--- create-phar.sh
+++ create-phar.sh
@@ -1,8 +1,27 @@
 #! /bin/sh
 
-ulimit -Sn 4096
-box=$(which box.phar)
-php -dphar.readonly=0 ${box} build || exit 1
-mv mcc.phar mcc
-echo "mcc has been created."
+export PATH=$PATH:.
 
+# Download composer if it does not exist:
+composer=$(which composer.phar)
+if [ -z ${composer} ]
+then
+  curl -sS https://getcomposer.org/installer | php
+fi
+
+# Download box if it does not exist:
+box=$(which box.phar)
+if [ -z ${box} ]
+then
+  curl -s http://box-project.org/installer.php | php
+fi
+
+# Increase maximum opened files, because box requires it:
+ulimit -Sn 4096
+
+# Build phar:
+rm -f mcc
+composer.phar install                         || exit 1
+php -dphar.readonly=0 $(which box.phar) build || exit 1
+mv mcc.phar mcc                               || exit 1
+echo "mcc has been created."
