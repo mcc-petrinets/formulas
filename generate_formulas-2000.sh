92f92b00411abea57cd84f3b5d4607ed3aee616b
Emmanuel Paviot-Adet <paviot@users.noreply.github.com>
2020-04-15 15:09:25 +0200

    Suppression de la génération des fichiers ReachabilityDeadlock.xml et ReachabilityDeadlock.txt

---
+++ generate_formulas-2000.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+MAX=2000
+ROOT=inputs-${MAX}states
+
+CATS="ReachabilityFireability
+ReachabilityCardinality
+UpperBounds
+CTLFireability
+CTLCardinality"
+
+function main ()
+{
+	echo "find(1) on the root directory:"
+	find $ROOT
+	echo
+	echo
+
+	for C in $CATS; do
+		NR=16
+		if test "$C" == "ReachabilityDeadlock"; then
+			NR=1
+		fi
+		echo ============================
+		echo "Category '$C'" depth=7 quantity=$NR
+		date
+		CMD="./mcc formula:generate --chain --subcategory=$C $ROOT --quantity=$NR --smc-max-states=$MAX --depth=7"
+		echo $CMD
+		time $CMD
+		echo "End of category '$C' exit status $?"
+	done
+	echo "END !!!!"
+	date
+}
+
+main > output-${MAX}states.txt 2>&1
+
